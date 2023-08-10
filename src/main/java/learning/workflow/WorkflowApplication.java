3ea7c1f93ad6412d4bed5886c879fb30791d0ce0
hpvlong <hophuocvietlong280999@gmail.com>
2023-08-10 11:31:31 +0700

    first commit

---
+++ src/main/java/learning/workflow/WorkflowApplication.java
@@ -0,0 +1,13 @@
+package learning.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowApplication.class, args);
+	}
+
+}
