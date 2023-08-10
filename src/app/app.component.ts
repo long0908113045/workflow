f50e86365fbb9e5f0410271da6b135661686133e
hpvlong <hophuocvietlong280999@gmail.com>
2023-08-10 16:11:04 +0700

    Update azure-pipelines.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import axios from 'axios';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'App';
-  ngOnInit(): void {}
 }
--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import {UserService} from "../user.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('HomeComponent', () => {
   let component: UserListComponent;
@@ -8,7 +11,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ UserListComponent ],
+      providers: [ UserService ]
     })
     .compileComponents();
 
